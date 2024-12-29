06ecbe4c9c11039463c4af095ce0a9ff192e66f8
Vainnor <crafty_chick.0u@icloud.com>
2024-12-28 21:01:55 -0500

    more system prefs

--- system-defaults.nix
+++ system-defaults.nix
@@ -7,5 +7,16 @@
       "/Applications/Discord.app"
       "/Applications/Cursor.app"
     ];
+    finder.FXPreferredViewStyle = "Nlsv";
+    finder.AppleShowAllFiles = true;
+    finder.NewWindowTarget = "Documents";
+    finder.ShowPathbar = true;
+    loginwindow.GuestEnabled = false;
+    NSGlobalDomain.AppleICUForce24HourTime = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    NSGlobalDomain.KeyRepeat = 2;
+    trackpad.TrackpadThreeFingerDrag = true;
+    WindowManager.EnableStandardClickToShowDesktop = true;
+    WindowManager.StandardHideDesktopIcons = true;
   };
 }
