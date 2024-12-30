262dd39f13a2b020dd8570716ee18220dd44fe4e
Vainnor <crafty_chick.0u@icloud.com>
2024-12-30 15:10:50 -0500

    dhabskjhfbahjs

--- hosts/vainnor-mac/configuration.nix
+++ hosts/vainnor-mac/configuration.nix
@@ -5,6 +5,7 @@
   config,
   ...
 }: {
+  
   # Add nix-homebrew configuration
   nix-homebrew = {
     enable = true;
@@ -141,6 +142,25 @@
 
     mkalias
   ];
+  system.activationScripts.applications.text = let
+  env = pkgs.buildEnv {
+    name = "system-applications";
+    paths = config.environment.systemPackages;
+    pathsToLink = "/Applications";
+  };
+in
+  pkgs.lib.mkForce ''
+  # Set up applications.
+  echo "setting up /Applications..." >&2
+  rm -rf /Applications/Nix\ Apps
+  mkdir -p /Applications/Nix\ Apps
+  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  while read -r src; do
+    app_name=$(basename "$src")
+    echo "copying $src" >&2
+    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+  done
+      '';
 
   # Zsh configuration
   programs.zsh.enable = true;
@@ -176,27 +196,6 @@
     ];
     onActivation.cleanup = "zap";
   };
-
-  system.activationScripts.applications.text = let
-  env = pkgs.buildEnv {
-    name = "system-applications";
-    paths = config.environment.systemPackages;
-    pathsToLink = "/Applications";
-  };
-in
-  pkgs.lib.mkForce ''
-  # Set up applications.
-  echo "setting up /Applications..." >&2
-  rm -rf /Applications/Nix\ Apps
-  mkdir -p /Applications/Nix\ Apps
-  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-  while read -r src; do
-    app_name=$(basename "$src")
-    echo "copying $src" >&2
-    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-  done
-      '';
-
   # Used for backwards compatibility, please read the changelog before changing.
   system.stateVersion = 5;
 }
